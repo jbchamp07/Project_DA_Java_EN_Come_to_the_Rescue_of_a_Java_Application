5b250392a3a166ce0410296cb337dffd75a94eee
jean-baptiste <jb.champetier@gmail.com>
2021-10-04 14:46:09 +0200

    add try catch update variable add hashset

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,80 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+	private static BufferedReader reader;
+	private static FileReader freader;
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+	public static void main(String args[]){
+		//BufferedReader reader = null;
+		try {
+			freader = new FileReader("D:\\ide\\projet formation\\Project02Eclipse\\symptoms.txt");
+			reader = new BufferedReader(freader);
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		String line = null;
+
+
+
+		Map<String, Integer> myMap = new HashMap<String, Integer>();
+			try {
+				do{
+					line = reader.readLine();
+					if(myMap.get(line) != null){
+						int nb = myMap.get(line) + 1;
+						myMap.put(line,nb);
+					}else{
+						myMap.put(line,1);
+					}
+				}while(line != null);
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+
+		List<Map.Entry<Integer, Integer>> list = new ArrayList(myMap.entrySet());
+		list.sort(Map.Entry.comparingByValue());
+		list.forEach(System.out::println);
+
+
+
+
+		/*ArrayList<Tableau> array = new ArrayList<>();
+		line = reader.readLine();
+		array.add(0,new Tableau(line,1));
+		for(int i = 0; line != null; i++){
+			line = reader.readLine();
+			if(array.contains(new Tableau(line))){
+				array.set(array.indexOf(new Tableau(line)),new Tableau(line,2));
+			}else{
+				array.add(new Tableau(line,1));
+			}
+		}
+		System.out.println(array.get(0).name + " : " + (array.get(0).nb));
+		System.out.println(array.get(1).name + " : " + (array.get(1).nb));*/
+
+
+		try {
+            //line = reader.readLine();	// get another symptom
+            reader.close();
+			freader.close();
+            // next generate output
+            FileWriter writer = null;
+			writer = new FileWriter("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Tableau.java
@@ -0,0 +1,13 @@
+package com.hemebiotech.analytics;
+
+public class Tableau {
+    String name;
+    int nb;
+    public Tableau(String nom, int nombre) {
+        nom = this.name;
+        nombre = this .nb;
+    }
+    public Tableau(String nom) {
+        nom = this.name;
+    }
+}
