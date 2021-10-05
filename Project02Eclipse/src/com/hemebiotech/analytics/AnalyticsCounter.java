96d20c019aeb83d3296eadd66ff34e4db3e3feb0
jean-baptiste <jb.champetier@gmail.com>
2021-10-05 12:49:42 +0200

    add methods

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,46 +4,27 @@ import java.io.*;
 import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
 	private static BufferedReader reader;
 	private static FileReader freader;
-	
+	private final static String filepath = "D:\\ide\\projet formation\\Project02Eclipse\\symptoms.txt";
+
 	public static void main(String args[]){
 		//BufferedReader reader = null;
 		try {
-			freader = new FileReader("D:\\ide\\projet formation\\Project02Eclipse\\symptoms.txt");
+			freader = new FileReader(filepath);
 			reader = new BufferedReader(freader);
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		String line = null;
 
 
 
 		Map<String, Integer> myMap = new HashMap<String, Integer>();
-			try {
-				do{
-					line = reader.readLine();
-					if(line != null){
-						if(myMap.get(line) != null){
-							int nb = myMap.get(line) + 1;
-							myMap.put(line,nb);
-						}else{
-							myMap.put(line,1);
-						}
-					}
-
-				}while(line != null);
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-			}
+		myMap = getSymptoms(myMap);
 
 		List<Map.Entry<Integer, Integer>> list = new ArrayList(myMap.entrySet());
-		list.sort(Map.Entry.comparingByValue());
+		list = getInOrder(list);
 		list.forEach(System.out::println);
 
 
@@ -62,14 +43,21 @@ public class AnalyticsCounter {
 		}
 		System.out.println(array.get(0).name + " : " + (array.get(0).nb));
 		System.out.println(array.get(1).name + " : " + (array.get(1).nb));*/
-
-
 		try {
-            //line = reader.readLine();	// get another symptom
-            reader.close();
+			reader.close();
 			freader.close();
-            // next generate output
-            FileWriter writer = new FileWriter("result.out");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		writeResult(list);
+
+	}
+
+	public static void writeResult(List<Map.Entry<Integer, Integer>> list){
+		try {
+			//line = reader.readLine();	// get another symptom
+			// next generate output
+			FileWriter writer = new FileWriter("result.out");
 			for(int i = 0; i < list.size(); i++){
 				writer.write(list.get(i).getKey() + " : " + list.get(i).getValue() + "\n");
 			}
@@ -77,6 +65,33 @@ public class AnalyticsCounter {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
+
+	public static Map<String, Integer> getSymptoms(Map<String, Integer> myMap){
+		String line = null;
+		try {
+			do{
+				line = reader.readLine();
+				if(line != null){
+					if(myMap.get(line) != null){
+						int nb = myMap.get(line) + 1;
+						myMap.put(line,nb);
+					}else{
+						myMap.put(line,1);
+					}
+				}
+
+			}while(line != null);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return myMap;
+	}
+
+	public static List<Map.Entry<Integer, Integer>> getInOrder(List<Map.Entry<Integer, Integer>> list){
+		list.sort(Map.Entry.comparingByValue());
+		return list;
+	}
+
 }
