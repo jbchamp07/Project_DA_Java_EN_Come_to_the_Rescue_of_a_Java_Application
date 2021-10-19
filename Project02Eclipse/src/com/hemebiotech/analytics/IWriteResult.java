a42bbb1e0e111de3ce9e102bae627f8e72ede9da
Champetier <jb.champetier@gmail.com>
2021-10-19 19:17:13 +0200

    Update JavaDoc

--- Project02Eclipse/src/com/hemebiotech/analytics/IWriteResult.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/IWriteResult.java
@@ -2,8 +2,16 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
+/**
+ * The interface Write result.
+ */
 public interface IWriteResult {
 
+    /**
+     * Write data.
+     *
+     * @param map the map
+     */
     void writeData(Map<String, Integer> map);
 
 }
